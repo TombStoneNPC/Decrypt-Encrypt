2d0f2c9e599f6bd0f4797e1df79546b510943f9f
TombStoneNPC <69434936+TombStoneNPC@users.noreply.github.com>
2020-08-28 12:57:51 +0200

    Add files via upload

---
+++ Decrypt.js
@@ -0,0 +1,25 @@
+function main() {
+    var parola, n;
+    var v, x, c, f;
+    var k;
+    
+    window.alert("Scrivi una chiave");
+    k = window.prompt('Enter a value for k');
+    window.alert("Scrivi una parola");
+    parola = window.prompt('Enter a value for parola');
+    v = parola.length();
+    for (x = 0; x <= v - 1; x++) {
+        n = parola.charAt(x);
+        window.alert(parola.charAt(x));
+        window.alert(" = ");
+        window.alert(String.CharCodeAt(n));
+        window.alert(" - ");
+        window.alert(k);
+        window.alert(" = ");
+        f = String.CharCodeAt(n);
+        c = f - k;
+        window.alert(c);
+        window.alert(" = ");
+        window.alert(String.fromCharCode(c));
+    }
+}
